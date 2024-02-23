36c41bf301dcfac2dbc8699e62c719eb5290aa82
Abhishekjain1997 <abhishekjain2589@gmail.com>
2024-02-23 11:29:31 +0530

    working

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import Button from './components/UI/Button/Button';
+
 import './App.css';
+import DemoOutput from './components/Demo/DemoOutput';
 
 function App() {
+  const[showparagraph,setshowparagraph]=useState(false);
+  console.log('APP RUNNING!!');
+
+  const toggleparagraphhandler=()=>{
+    setshowparagraph((prevshowparagraph) =>!prevshowparagraph)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Hi there!</h1>
+      <DemoOutput show={false}/>
+      <Button onClick={toggleparagraphhandler}>Toggle Paragraph!</Button>
+     
     </div>
   );
 }
